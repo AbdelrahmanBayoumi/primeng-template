aa73069b23570a1b720b70b63d1fa6655f396aa6
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2024-01-25 20:31:42 +0200

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ROUTES } from './app.routes';
+import { AppLayoutModule } from './layout/app.layout.module';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom(BrowserModule, AppLayoutModule),
+    provideRouter(ROUTES),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
-export const routes: Routes = [];
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
+  },
+];
