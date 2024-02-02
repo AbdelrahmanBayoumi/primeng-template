9fcb83180e71e6518897ec4635dfd61453a54eca
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2024-02-02 16:38:16 +0200

    update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'prime-template';
-}
+export class AppComponent {}
